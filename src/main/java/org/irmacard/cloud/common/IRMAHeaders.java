3d3535d28801b804b3c0fb7c2665d0477f3babe5
Wouter Lueks <wouter@telox.net>
2016-09-22 11:57:31 +0200

    Some more common files

---
+++ src/main/java/org/irmacard/cloud/common/IRMAHeaders.java
@@ -0,0 +1,6 @@
+package org.irmacard.cloud.common;
+
+public class IRMAHeaders {
+	public static final String USERNAME = "IRMA_Username";
+	public static final String AUTHORIZATION = "IRMA_Authorization";
+}
--- src/main/java/org/irmacard/cloud/common/UserMessage.java
+++ src/main/java/org/irmacard/cloud/common/UserMessage.java
@@ -5,14 +5,16 @@ public class UserMessage {
 		private String sessionToken;
 		private String ID;
 		private boolean enrolled;
+		private boolean enabled;
 
 		public UserMessage() {};
 
-		public UserMessage(String username, String sessionToken, String ID, boolean enrolled) {
+		public UserMessage(String username, String sessionToken, String ID, boolean enrolled, boolean enabled) {
 			this.username = username;
 			this.sessionToken = sessionToken;
 			this.ID = ID;
 			this.enrolled = enrolled;
+			this.setEnabled(enabled);
 		}
 
 		public String getUsername() {
@@ -45,5 +47,13 @@ public class UserMessage {
 
 		public void setEnrolled(boolean enrolled) {
 			this.enrolled = enrolled;
+		}
+
+		public boolean isEnabled() {
+			return enabled;
+		}
+
+		public void setEnabled(boolean enabled) {
+			this.enabled = enabled;
 		}
 }
