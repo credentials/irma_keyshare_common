813378f4c318257124f15c0058f1ec7be57a9b30
Wouter Lueks <wouter@telox.net>
2016-09-22 11:57:31 +0200

    Some more common files

---
+++ src/main/java/org/irmacard/cloud/common/AuthorizationResult.java
@@ -0,0 +1,47 @@
+package org.irmacard.cloud.common;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class AuthorizationResult {
+	String status;
+	List<String> candidates;
+
+	public static final String STATUS_AUTHORIZED = "authorized";
+	public static final String STATUS_EXPIRED = "expired";
+	public static final String STATUS_BLOCKED = "blocked";
+
+	public AuthorizationResult() {
+	}
+
+	public AuthorizationResult(String status, List<String> candidates) {
+		this.status = status;
+		this.candidates = candidates;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public List<String> getCandidates() {
+		return candidates;
+	}
+
+	public void setCandidates(List<String> candidates) {
+		this.candidates = candidates;
+	}
+
+	public String toString() {
+		String result = "Status: " + status + ", candidates: [";
+		if(candidates != null) {
+			result += Arrays.toString(candidates.toArray()) + "]";
+		} else {
+			result += "<<null>>";
+		}
+		return result;
+	}
+}
