86386e9a767f8a78012a1a21b623f979f8b97655
Sietse Ringers <mail@sietseringers.net>
2017-04-18 19:53:43 +0200

    Add KeyshareError for occopied username

--- src/main/java/org/irmacard/keyshare/common/exceptions/KeyshareError.java
+++ src/main/java/org/irmacard/keyshare/common/exceptions/KeyshareError.java
@@ -20,6 +20,7 @@ public enum KeyshareError {
 	USER_BLOCKED(403, "User is blocked"),
 	USER_NOT_REGISTERED(403, "User is not yet fully registered"),
 	UNAUTHORIZED(403, "Unauthorized, authenticate to the server first"),
+	USERNAME_UNAVAILABLE(409, "E-mailaddress unavailable"),
 
 	// webclient related exceptions
 	USER_NOT_FOUND(404, "Cannot find user"),
